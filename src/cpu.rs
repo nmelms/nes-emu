194ba7de6e4de3483e03ca0cf669ed28d84a3f8a
Nick Melms <nmelms92@gmail.com>
2026-02-18 20:49:15 -0800

    create cpu module

---
+++ src/cpu.rs
@@ -0,0 +1,17 @@
+
+
+pub struct CPU {
+    // registers
+    // accumulator
+    a: u8,
+    // indexes
+    x: u8,
+    y: u8,
+    // stack pointer
+    sp: u8,
+    // status
+    p: u8,
+    // program counter
+    pc: u16,
+    
+}
